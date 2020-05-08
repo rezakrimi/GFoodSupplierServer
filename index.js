4267956ea6e166aba61a67e8f20c4f17989ee22b
Reza Karimi <rezakarimi@google.com>
2020-05-08 12:20:42 -0400

    creating static database

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 
+const db = {
+    "yeast": ["vendorA", "vendorB"],
+    "flour": ["vendorC", "vendorD"],
+    "butter": ["vendorB"],
+    "egg": ["vendorA", "vendorB", "vendorC", "vendorD"],
+    "milk": ["vendorA", "vendorC", "vendorD"],
+    "sugar": ["vendorB", "vendorD"]
+};
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send(db["egg"]);
 });
 
 app.listen(8000, '0.0.0.0', () => {
-  console.log('Example app listening on port 8000!')
+    console.log('Example app listening on port 8000!');
 });
