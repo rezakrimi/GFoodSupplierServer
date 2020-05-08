3e18cc82c4c2516782d6cc524b3de42a9c2ccfa7
Reza Karimi <rezakarimi@google.com>
2020-05-08 11:23:07 -0400

    setting up the server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(8000, '0.0.0.0', () => {
+  console.log('Example app listening on port 8000!')
+});
